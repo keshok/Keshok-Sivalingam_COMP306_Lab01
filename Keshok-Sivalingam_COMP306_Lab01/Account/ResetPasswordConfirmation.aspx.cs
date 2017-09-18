c5fcf03f9d8e427283ccd23bd771566df991778f
Keshok Sivalingam <keshok@live.ca>
2017-09-18 15:06:30 -0400

    Add project files.

---
+++ Keshok-Sivalingam_COMP306_Lab01/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Keshok_Sivalingam_COMP306_Lab01.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Keshok-Sivalingam_COMP306_Lab01/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Keshok_Sivalingam_COMP306_Lab01.Startup))]
+namespace Keshok_Sivalingam_COMP306_Lab01
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
